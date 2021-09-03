3e64f8cf9e402fdda73697bad9de46e4fca79253
Dominic Lewis <dlewis@onion.fios-router.home>
2021-09-03 02:32:22 -0400

    Finished full channel archive, fixed requirements.txt

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ RSS = https://www.youtube.com/feeds/videos.xml?channel_id= + channel_id
 """
 
 import time
-from multiprocessing import Process
+from multiprocessing import Process, Pool
 import notify_run
 import youtube_dl
 import requests
@@ -22,8 +22,12 @@ import os
 import feedparser
 import unicodedata
 import re
+import datetime
 
-
+def log(message: str):
+    with open("log.txt", "a", encoding="UTF-8") as logfile:
+        logfile.write("<" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "> " + message + "\n")
+    print(message)
 
 def slugify(value: str, allow_unicode=True): #used to sanitize string for filesystem
     value = str(value)
@@ -46,8 +50,11 @@ class channel():
         with open(os.path.join("channels", slugify(self.name) + ".channel"), "w") as outfile:
             outfile.write(self.name + "\n" + self.channel_id + "\n" + self.status)
 
-    def getRSS(self): #returns channel's RSS feed as a list of entries
+    def get_rss(self): #returns channel's RSS feed as a list of entries
         return feedparser.parse(requests.get("https://www.youtube.com/feeds/videos.xml?channel_id=" + self.channel_id).text).entries
+
+    def get_all_plist(self):
+        return "https://www.youtube.com/channel/" + self.channel_id
 
 def load_channels():
     channels = list()
@@ -64,10 +71,95 @@ def load_keywords():
             keywords.append(line)
     return keywords
 
-def ASMRchive(channels: list, keywords: list):
-    pass
+def get_video(url, ydl_opts):
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download((url,))
+            return True
+    except Exception as e:
+        return False
+        
+
+def ASMRchive(channels: list, keywords: list, output_directory: str):
+    for chan in channels:
+        if chan.status == "new": #we want to do a full archive of all videos
+            to_download = list()
+            ydl_opts = {
+                'nocheckcertificate': True,
+                'ignoreerrors': True,
+                'extract_flat': "in_playlist",
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl: #This one gets the playlists
+                meta = ydl.extract_info(chan.get_all_plist(), download=False)
+            ydl_opts = {
+                'nocheckcertificate': True,
+                'ignoreerrors': True,
+                'extract_flat': True,
+            }
+            for item in meta["entries"]:
+                if item["title"] == "Uploads":
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl: #This one gets the videos from the 'uploads' playlist (all uploads hopefully.)
+                        meta = ydl.extract_info(item["url"], download=False)
+                        break
+            for video in meta["entries"]:
+                for word in keywords:
+                    if not ("https://www.youtube.com/watch?v=" + video["id"]) in to_download:
+                        if word.lower() in video["title"].lower():
+                            to_download.append([video["title"] ,"https://www.youtube.com/watch?v=" + video["id"]])
+                            break
+            ydl_opts = {
+                'nocheckcertificate': True,
+                'writethumbnail': True,
+                'format': "bestaudio/best",
+                "writedescription": True,
+                "writeinfojson": True,
+            }
+            processes = []
+            for video in to_download:
+                if not os.path.exists(os.path.join(output_directory, slugify(chan.name), slugify(video[0]))):
+                    os.makedirs(os.path.join(output_directory, slugify(chan.name), slugify(video[0])))
+                ydl_opts['outtmpl'] = os.path.join(output_directory, slugify(chan.name), slugify(video[0]), '%(title)s.%(ext)s')
+                p = Process(target=get_video, args=(video[1], ydl_opts))
+                p.start()
+                processes.append(p)
+            TIMEOUT = 60
+            time_spent = 0
+            alive = True
+            while alive: #forgive my handling of these processes... (youtube throttles sometimes, this is to 'refresh' that throttling)
+                alive = False
+                for p in processes:
+                    if p.is_alive():
+                        alive = True
+                        break
+                if alive:
+                    time.sleep(5)
+                    time_spent = time_spent + 5
+                    if time_spent >= TIMEOUT:
+                        time_spent = 0
+                        for p in processes:
+                            if p.is_alive():
+                                p.terminate()
+                                p.join()
+                        processes = []
+                        for video in to_download:
+                            if not os.path.exists(os.path.join(output_directory, slugify(chan.name), slugify(video[0]))):
+                                os.makedirs(os.path.join(output_directory, slugify(chan.name), slugify(video[0])))
+                            ydl_opts['outtmpl'] = os.path.join(output_directory, slugify(chan.name), slugify(video[0]), '%(title)s.%(ext)s')
+                            p = Process(target=get_video, args=(video[1], ydl_opts))
+                            p.start()
+                            processes.append(p)
+            chan.status = "archived"
+            chan.save()
+        elif chan.status == "archived": #we want to check the RSS for new ASMR streams
+            pass
+        else: #reserved for un-statused channels. Not sure what this will be for.
+            pass
 
 if __name__ == "__main__":
     channels = load_channels()
     keywords = load_keywords()
-    ASMRchive(channels, keywords)
+    for key in keywords:
+        print(key)
+    #output_directory = "E:\Youtube"
+    output_directory = "/mnt/thicc/youtube"
+    ASMRchive(channels, keywords, output_directory)
