244acdb56e1c139879deaaabc9c734e56d2dc8bc
Dominic Lewis <60369937+whgDosumi@users.noreply.github.com>
2023-04-19 01:47:15 -0400

    Revert "Fix ytchanges (#47)"
    This reverts commit c67c9a9993c0d60ca748e81f704e32de22141811.

--- startup.sh
+++ startup.sh
@@ -1,10 +1,5 @@
 crond
 php-fpm
-rm -f /var/www/html/ASMR
-rm -f /var/www/html/channels
-rm -f /var/python_app/channels/channels
-ln -s /var/ASMRchive /var/www/html/ASMR
-ln -s /var/python_app/channels /var/www/html/channels
 python /var/python_app/update_web.py
 python /var/python_app/main.py bypass_convert
 /usr/sbin/httpd -D FOREGROUND
