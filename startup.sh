c67c9a9993c0d60ca748e81f704e32de22141811
Dominic Lewis <60369937+whgDosumi@users.noreply.github.com>
2023-04-19 01:36:50 -0400

    Fix ytchanges (#47)
    * Fix new issues
    
    This updates the code to properly handle youtubes new channel layout. Fixes new entries omitting lives and shorts, and only grabbing the uploaded videos. Now searches everything like intended.
    
    * Add symbolic links to startup
    
    Fixes some filesystem issues that arise when creating a new instance and not using a legacy build of ASMRchive.

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 crond
 php-fpm
+rm -f /var/www/html/ASMR
+rm -f /var/www/html/channels
+rm -f /var/python_app/channels/channels
+ln -s /var/ASMRchive /var/www/html/ASMR
+ln -s /var/python_app/channels /var/www/html/channels
 python /var/python_app/update_web.py
 python /var/python_app/main.py bypass_convert
 /usr/sbin/httpd -D FOREGROUND
