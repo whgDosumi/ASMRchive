a3584063634eceacd6e8bc5cef1268cbb7593aca
whgDosumi <whgdomi@gmail.com>
2023-08-08 18:16:18 -0400

    Changed to .appdata, confirmed webserver works

--- startup.sh
+++ startup.sh
@@ -3,11 +3,13 @@
 crond
 php-fpm
 
-if [ ! -d "/var/ASMRchive/appdata" ] # If initial appdata isn't imported, import it. 
+if [ ! -d "/var/ASMRchive/.appdata" ] # If initial appdata isn't imported, import it. 
 then
-    mkdir "/var/ASMRchive/appdata"
-    mv /var/python_app/channels /var/ASMRchive/appdata/
-    mv /var/python_app/cookies /var/ASMRchive/appdata/
+    mkdir "/var/ASMRchive/.appdata"
+    mv /var/python_app/channels /var/ASMRchive/.appdata/
+    ln -s /var/ASMRchive/.appdata/channels /var/www/html/channels # Give webserver access to channels. 
+    mv /var/python_app/cookies /var/ASMRchive/.appdata/
+    chmod o-r /var/ASMRchive/appdata/cookies # This is to prevent the webserver from having access to the cookie files.
 fi
 
 python /var/python_app/update_web.py
