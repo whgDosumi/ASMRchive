ae2f2c5d515d710c6e93f5b30af99d6120c80bb5
whgDosumi <whgdomi@gmail.com>
2023-08-09 22:09:55 -0400

    Fix channel symlink issue

--- startup.sh
+++ startup.sh
@@ -10,8 +10,10 @@ then
     mv /var/python_app/cookies /var/ASMRchive/.appdata/
     chmod o-r /var/ASMRchive/.appdata/cookies # This is to prevent the webserver from having access to the cookie files.
 fi
+if [ ! -L "/var/www/html/channels" ]
+then
 ln -s /var/ASMRchive/.appdata/channels /var/www/html/channels # Give webserver access to channels. 
-
+fi
 python /var/python_app/update_web.py
 python /var/python_app/main.py bypass_convert
 /usr/sbin/httpd -D FOREGROUND
