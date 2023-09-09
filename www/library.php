c825d552ed23359fefbcf6409d5abc55cda94f10
Dominic Lewis <60369937+whgDosumi@users.noreply.github.com>
2023-09-09 04:01:51 -0400

    Reprioritize Wav

--- www/library.php
+++ www/library.php
@@ -6,7 +6,7 @@
     # These are the supported audio formats
     # Check with Dominic before updating these, to ensure the conversion will work.
     # The order is the order of preference the client will use if all are present.
-    $SUPPORTED_FORMATS = array(".webm", ".flac", ".m4a", ".opus", ".mp3", ".wav");
+    $SUPPORTED_FORMATS = array(".wav", ".webm", ".flac", ".m4a", ".opus", ".mp3");
 
     header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
     header("Pragma: no-cache"); // HTTP 1.0.
