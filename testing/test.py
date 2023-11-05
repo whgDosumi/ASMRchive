91094a0d521754324dbdc407ffbf26cebadbbfc6
whgDosumi <whgdomi@gmail.com>
2023-11-05 00:59:49 -0400

    Add some automated testing

--- testing/test.py
+++ testing/test.py
@@ -9,13 +9,42 @@ import requests
 import time
 import shutil
 
-# my channel id: UC1kvM3pZGg3QaSQBS91Cwzg
+test_channel_name = "Dom"
+test_channel_id = "UC1kvM3pZGg3QaSQBS91Cwzg"
 
 # Define chrome options
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
+
+class Channel():
+    def __init__(self, name, status, count) -> None:
+        self.name = name
+        self.status = status
+        self.count = int(count)
+
+def load_channels(webpage_text): # creates channel objects by reading the homepage url
+    channels = []
+    start = 0
+    end = 0
+    while end != -1:
+        start = webpage_text.find("class=\"channel\"", start)
+        start = webpage_text.find(">", start) + 1
+        end = webpage_text.find("</", start)
+        name = webpage_text[start:end]
+        start = webpage_text.find("class=\"status\"", start)
+        start = webpage_text.find(">", start) + 1
+        end = webpage_text.find("</", start)
+        status = webpage_text[start:end]
+        start = webpage_text.find("class=\"count\"", start)
+        start = webpage_text.find(">", start) + 1
+        end = webpage_text.find("</", start)
+        count = webpage_text[start:end]
+        channels.append(Channel(name, status, count))
+        end = webpage_text.find("class=\"channel\"", start)
+    return channels
+
 # Allow a port override in case we're testing on a dev machine
 if os.path.exists("/test/port_override.txt"):
     try:
@@ -26,13 +55,49 @@ if os.path.exists("/test/port_override.txt"):
 else:
     test_port = 4445
 
-script_directory = os.path.dirname(os.path.abspath(__file__))
-
 homepage_url = f"http://localhost:{test_port}"
 admintools_url = f"http://localhost:{test_port}/admintools.php"
+script_directory = os.path.dirname(os.path.abspath(__file__))
 print(homepage_url)
 #Initialize chrome webdriver
 web = webdriver.Chrome(options=chrome_options)
 # Get our main pages, ensure we can connect properly
 web.get(homepage_url)
+
 web.get(admintools_url)
+
+# Add test channel
+web.find_element(By.ID, "channel_name").send_keys(test_channel_name)
+web.find_element(By.ID, "channel_id").send_keys(test_channel_id)
+web.find_element(By.ID, "add_channel_button").click()
+alert = WebDriverWait(web, 10).until(EC.alert_is_present())
+assert "Channel added" in alert.text
+alert.accept()
+# Force a scan
+web.find_element(By.ID, "force-scan").click()
+alert = WebDriverWait(web, 10).until(EC.alert_is_present())
+assert "Forcing ASMR Scan." in alert.text
+alert.accept()
+
+# Wait for the new channel to appear, and for the video to download
+max_retries = 10
+refresh_rate = 5
+tries = 0
+passed = False # To track wether the test passed or failed
+while tries < max_retries:
+    tries += 1
+    web.get(homepage_url)
+    channels = load_channels(web.page_source)
+    test_channel = None
+    for channel in channels:
+        if channel.name == test_channel_name:
+            test_channel = channel
+    if test_channel.status == "Archived" and test_channel.count >= 1:
+        passed = True
+        break
+assert passed
+            
+
+
+# Declare proudly that our testing has passed
+print("All automated tests have passed.")
