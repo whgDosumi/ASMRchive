1f11da164caa1919c617238ed5253f874dbdb8b0
Dominic Lewis <60369937+whgDosumi@users.noreply.github.com>
2024-05-24 04:07:34 -0400

    test: Add more logging (#104)

--- python_app/force_scan.sh
+++ python_app/force_scan.sh
@@ -4,7 +4,7 @@
 for i in {1..12}; do
     if [ -f "/var/www/html/flags/scan_flag.txt" ]; then
         rm -f /var/www/html/flags/scan_flag.txt
-        python /var/python_app/main.py
+        python /var/python_app/main.py >> "/var/ASMRchive/.appdata/logs/python/main-$(date +\%Y-\%m-\%d)-asmr.log" 2>&1
     fi
     sleep 5
 done
